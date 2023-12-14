22c205ed7cbdef24e6b3fd4639b7902791f4ab4a
gmifflen <115088238+gmifflen@users.noreply.github.com>
2023-12-14 03:15:15 -0400

    Update main.rs
    added level-based role support, and top command for it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 use serenity::{
+    all::RoleId,
     async_trait,
     framework::standard::{
         macros::{command, group},
@@ -49,8 +50,28 @@ impl Handler {
         .unwrap();
     }
 
+    const BASE_XP: u32 = 50;
+    const GROWTH_RATE: f64 = 1.15;
+
+    fn calculate_xp_for_level(level: u32) -> u32 {
+        (Self::BASE_XP as f64 * (1.0 + Self::GROWTH_RATE).powi(level as i32 - 1)) as u32
+    }
+
     fn calculate_level(xp: u32) -> u32 {
-        (0.1 * (xp as f64).sqrt()) as u32
+        let mut level = 1;
+        while Self::calculate_xp_for_level(level) <= xp {
+            level += 1;
+        }
+        level - 1 // Subtract 1 because level will be one higher than the level for the given XP
+    }
+
+    async fn get_top_users(pool: &MySqlPool) -> Vec<(u64, u32)> {
+        sqlx::query_as::<_, (u64, u32)>(
+            "SELECT user_id, level FROM user_xp ORDER BY level DESC LIMIT 10",
+        )
+        .fetch_all(pool)
+        .await
+        .unwrap_or_else(|_| vec![])
     }
 }
 
@@ -62,9 +83,8 @@ impl EventHandler for Handler {
         }
 
         let data_read = ctx.data.read().await;
-        let pool = data_read.get::<MySqlPoolContainer>().unwrap().pool;
-
-        let user_id = msg.author.id.get() as u64;
+        let pool = &data_read.get::<MySqlPoolContainer>().unwrap().pool.clone();
+        let user_id = u64::from(msg.author.id);
         let xp_gain = rand::thread_rng().gen_range(1..=10);
         let (current_xp, current_level) = Handler::get_user_xp(user_id, &pool).await;
         let new_level = Handler::calculate_level(current_xp + xp_gain);
@@ -80,6 +100,54 @@ impl EventHandler for Handler {
                 .await
             {
                 println!("Error sending message: {:?}", why);
+            }
+
+            // Role assignment logic
+            let guild_id = match msg.guild_id {
+                Some(guild_id) => guild_id,
+                None => return,
+            };
+
+            let member = match guild_id.member(&ctx.http, msg.author.id).await {
+                Ok(member) => member,
+                Err(_) => return,
+            };
+
+            let data_read = ctx.data.read().await;
+            let roles = data_read.get::<RoleIds>().unwrap();
+
+            let all_role_ids = vec![
+                RoleId::from(roles.beginner),
+                RoleId::from(roles.rookie),
+                RoleId::from(roles.intermediate),
+                RoleId::from(roles.advanced),
+                RoleId::from(roles.expert),
+                RoleId::from(roles.master),
+                RoleId::from(roles.elite),
+            ];
+
+            // Remove existing level roles
+            for role_id in &all_role_ids {
+                if member.roles.contains(role_id) {
+                    if let Err(why) = member.remove_role(&ctx.http, *role_id).await {
+                        println!("Error removing role: {:?}", why);
+                    }
+                }
+            }
+
+            // Assign new role
+            let new_role_id = match new_level {
+                1..=5 => RoleId::from(roles.beginner),
+                6..=10 => RoleId::from(roles.rookie),
+                11..=20 => RoleId::from(roles.intermediate),
+                21..=30 => RoleId::from(roles.advanced),
+                31..=40 => RoleId::from(roles.expert),
+                41..=50 => RoleId::from(roles.master),
+                _ => RoleId::from(roles.elite),
+            };
+
+            if let Err(why) = member.add_role(&ctx.http, new_role_id).await {
+                println!("Error assigning role: {:?}", why);
             }
         }
     }
@@ -97,13 +165,27 @@ impl TypeMapKey for MySqlPoolContainer {
     type Value = MySqlPoolContainer;
 }
 
+struct RoleIds {
+    beginner: u64,
+    rookie: u64,
+    intermediate: u64,
+    advanced: u64,
+    expert: u64,
+    master: u64,
+    elite: u64,
+}
+
+impl TypeMapKey for RoleIds {
+    type Value = RoleIds;
+}
+
 #[group]
-#[commands(ping)]
+#[commands(ping, top)]
 struct General;
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(&ctx.http, "Pong!").await?;
+    msg.channel_id.say(&ctx.http, "I'm still alive!").await?;
     Ok(())
 }
 
@@ -116,6 +198,41 @@ async fn main() {
         .expect("Could not connect to the database");
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let beginner_role_id = env::var("BEGINNER_ROLE_ID")
+        .expect("Expected BEGINNER_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("BEGINNER_ROLE_ID must be a valid u64");
+
+    let rookie_role_id = env::var("ROOKIE_ROLE_ID")
+        .expect("Expected ROOKIE_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("ROOKIE_ROLE_ID must be a valid u64");
+
+    let intermediate_role_id = env::var("INTERMEDIATE_ROLE_ID")
+        .expect("Expected INTERMEDIATE_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("INTERMEDIATE_ROLE_ID must be a valid u64");
+
+    let advanced_role_id = env::var("ADVANCED_ROLE_ID")
+        .expect("Expected ADVANCED_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("ADVANCED_ROLE_ID must be a valid u64");
+
+    let expert_role_id = env::var("EXPERT_ROLE_ID")
+        .expect("Expected EXPERT_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("EXPERT_ROLE_ID must be a valid u64");
+
+    let master_role_id = env::var("MASTER_ROLE_ID")
+        .expect("Expected MASTER_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("MASTER_ROLE_ID must be a valid u64");
+
+    let elite_role_id = env::var("ELITE_ROLE_ID")
+        .expect("Expected ELITE_ROLE_ID in the environment")
+        .parse::<u64>()
+        .expect("ELITE_ROLE_ID must be a valid u64");
 
     let http = Http::new(&token);
 
@@ -145,9 +262,48 @@ async fn main() {
     {
         let mut data = client.data.write().await;
         data.insert::<MySqlPoolContainer>(MySqlPoolContainer { pool });
+        data.insert::<RoleIds>(RoleIds {
+            beginner: beginner_role_id,
+            rookie: rookie_role_id,
+            intermediate: intermediate_role_id,
+            advanced: advanced_role_id,
+            expert: expert_role_id,
+            master: master_role_id,
+            elite: elite_role_id,
+        });
     }
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+
+#[command]
+async fn top(ctx: &Context, msg: &Message) -> CommandResult {
+    let data_read = ctx.data.read().await;
+    let pool = data_read.get::<MySqlPoolContainer>().unwrap().pool.clone();
+
+    let top_users = Handler::get_top_users(&pool).await;
+    let mut response = String::new();
+
+    for (index, (user_id, level)) in top_users.into_iter().enumerate() {
+        let user_name = match ctx.http.get_user(user_id.into()).await {
+            Ok(user) => user.name,
+            Err(_) => format!("Unknown User ({})", user_id),
+        };
+        response.push_str(&format!(
+            "{}. {} - Level: {}\n",
+            index + 1,
+            user_name,
+            level
+        ));
+    }
+
+    if !response.is_empty() {
+        msg.channel_id.say(&ctx.http, &response).await?;
+    } else {
+        msg.channel_id.say(&ctx.http, "No users found.").await?;
+    }
+
+    Ok(())
 }
